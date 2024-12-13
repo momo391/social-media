240495fa391160001d25416f021fb5d876f7b03e
momo391 <ia7636hs@gmail.com>
2024-12-13 17:46:12 +0100

    cookies api impletemented with middleware

---
+++ lib/server/cookies.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+
+export async function setSessionTokenCookie(
+  token: string,
+  expires_at: Date
+): Promise<void> {
+  const cookieStore = await cookies();
+  cookieStore.set("session", token, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    expires: expires_at,
+    path: "/",
+  });
+}
+
+export async function delelteSessionTokenCookie(): Promise<void> {
+  const cookieStore = await cookies();
+  cookieStore.set("session", "", {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 0,
+    path: "/",
+  });
+}
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
+  // cookies
+  if (request.method === "GET") {
+    const response = NextResponse.next();
+    const token = request.cookies.get("session")?.value ?? null;
+    if (token !== null) {
+      response.cookies.set("session", token, {
+        path: "/",
+        maxAge: 60 * 60 * 2,
+        sameSite: "lax",
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      });
+    }
+    return response;
+  }
+
+  //  CSRF protection
   if (request.method === "GET") return NextResponse.next();
 
   const originHeader = request.headers.get("Origin");
+  // change to `X-Forwarded-Host` later if not working
   const hostHeader = request.headers.get("Host");
 
   if (originHeader === null || hostHeader === null)
