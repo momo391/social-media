50899cf838c7cfbb46a2e1a10d5d402b879b78a5
momo391 <ia7636hs@gmail.com>
2024-12-18 12:59:21 +0100

    route error removed

--- app/error/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function ErrorLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <>
-      <div className="min-h-screen grid  place-items-center">{children}</div>
-    </>
-  );
-}
